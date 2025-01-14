c238908fc7229c7aa972852398f851dc4afde5a5
Zie <ndlovuzibusiso@gmail.com>
2025-01-14 15:36:30 +0200

    Corrected bucket name

--- Terraform_AWS_Services/terraform-state-s3-locking-bucket/variables.tf
+++ Terraform_AWS_Services/terraform-state-s3-locking-bucket/variables.tf
@@ -1,7 +1,7 @@
 variable "s3_bucket_name" {
   description = "S3 Bucket name"
   type        = string
-  default     = "K8s-Automated-cluster-deployment-01-2025"
+  default     = "k8s-automated-cluster-deployment-01-2025"
 }
 
 variable "aws_region" {
